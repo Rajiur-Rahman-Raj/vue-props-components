e0e39df2ac7b0ebb7c7ea90905bd96ffe4ecfa89
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-08-15 22:53:36 +0600

    todo list with props and components

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,71 @@
 <script setup>
-  
-  import BookTicket from './components/BookTicket.vue'
+    import TodoList from "./components/TodoList.vue";
 
-  const topHeading = "Components"
-
-  const bookingInfos = [{
-    title:"The Magnificent Bogra",
-    distance:"196 km",
-    fromAddress:"from Dhaka",
-    reviews:5,
-    reviewCount:10,
-    button:"Book Ticket",
-    location:"*Places to visit: Mahasthangarh, Vasu Bihar & Momo Inn",
-    image:"https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg"
-  },
-  {
-    title:"The Magnificent Bogra",
-    subTitle:"Located in Rajshahi Division, Bogra is one of the oldest and most fascinating towns in Bangladesh",
-    distance:"196 km",
-    fromAddress:"from Dhaka",
-    reviews:4,
-    reviewCount:15,
-    button:"Book Ticket",
-    location:"*Places to visit: Mahasthangarh, Vasu Bihar & Momo Inn",
-    image:"https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg"
-  }
-]
+    const myTasks = [{
+      title:"Frodo's Jobs",
+      background: "bg-white",
+      lists:[
+        {
+          task:"Weed front garden.",
+          status: true,
+        },
+        {
+          task:"Chill and smoke some Old Toby.",
+          status: true,
+        },
+        {
+          task:"Keep ring secret and safe.",
+          status: false,
+        },
+        {
+          task:"Meet Gandalf at Bree.",
+          status: false,
+        },
+        {
+          task:"Destroy ring and defeat dark lord.",
+          status: false,
+        },
+    ]
+    },
+    {
+      title:"Sam's Jobs",
+      background: "bg-gray-800",
+      lists:[
+        {
+          task:"Trim the verge.",
+          status: true,
+        },
+        {
+          task:"Eavesdrop on Master Frodo & Gandalf.",
+          status: true,
+        },
+        {
+          task:"Boil, mash, and stick potatoes in stew.",
+          status: false,
+        },
+        {
+          task:"Carry Frodo.",
+          status: false,
+        },
+        {
+          task:"Be all round legend.",
+          status: false,
+        },
+    ]
+    }
+  ]
 
 </script>
 
-
 <template>
   <section class="mx-auto ">
-    <h1 class="my-10 text-center">{{ topHeading }}</h1>
-    <div class="flex flex-col space-y-5">
-      <BookTicket :bookinginformation="bookingInfos"/>
+    <h1 class="my-10 text-center">Components</h1>
+    <div class="flex items-center justify-center h-[600px] font-medium">
+      <TodoList :tasks="myTasks" background1="defaultBg1" background2="defaultBg2"/>
     </div>
   </section>
 </template>
 
 <style scoped>
-
+  
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,66 @@
+<script setup>
+    defineProps({
+        tasks: Array,
+    })
+</script>
+
+<template>
+    <div class="flex flex-grow items-center justify-center h-full text-gray-600 bg-gray-100">
+        <!-- Component Start -->
+        <div class="max-w-full p-8 rounded-lg shadow-lg w-96" :class="tasks[0].background">
+            <div class="flex items-center mb-6">
+                <svg class="h-8 w-8 text-indigo-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path>
+                                    </svg>
+                <h4 class="font-semibold ml-3 text-lg">Frodo's Jobs</h4>
+            </div>
+            <div v-for="(list, index) in tasks[0].lists" :key="index">
+                <input class="hidden" type="checkbox" :id="`task${index}`" :checked="list.status" >
+                <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-100" :for="`task${index}`">
+                                      <span class="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full">
+                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                                        </svg>
+                                      </span>
+                                      <span class="ml-4 text-sm">{{ list.task }}.</span>
+                                    </label>
+            </div>
+        </div>
+        <!-- Component End  -->
+    </div>
+    <div class="flex flex-grow items-center justify-center bg-gray-900 h-full">
+        <!-- Component Start -->
+        <div class="max-w-full p-8  rounded-lg shadow-lg w-96 text-gray-200" :class="tasks[1].background">
+            <div class="flex items-center mb-6">
+                <svg class="h-8 w-8 text-indigo-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path>
+                                    </svg>
+                <h4 class="font-semibold ml-3 text-lg">Sam's Jobs</h4>
+            </div>
+            <div v-for="(list, index) in tasks[0].lists" :key="index">
+                <input class="hidden" type="checkbox" :id="`task${list.task}`" :checked="list.status">
+                <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900" :for="`task${list.task}`">
+                                      <span class="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
+                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+                                        </svg>
+                                      </span>
+                                      <span class="ml-4 text-sm">{{ list.task }}</span>
+                                    </label>
+            </div>
+        </div>
+        <!-- Component End  -->
+    </div>
+</template>
+
+<style>
+    input[type=checkbox]:checked+label span:first-of-type {
+        background-color: #10B981;
+        border-color: #10B981;
+        color: #fff;
+    }
+    input[type=checkbox]:checked+label span:nth-of-type(2) {
+        text-decoration: line-through;
+        color: #9CA3AF;
+    }
+</style>
